fd056ec5f28b714735828167a832269f74e3cb93
Demian <demyan.korotchenko@gmail.com>
2023-09-09 15:20:12 +0200

    localstorageClear

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from './Form/Form';
 import { ContactsList } from './ContactList/ContactsList';
 import { Filter } from './Filter/Filter';
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    return storedContacts ? JSON.parse(storedContacts) : initialContacts;
+  });
   const [filter, setFilter] = useState('');
-  // filter: ''
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleAddContact = newContact => {
     const isExist = contacts.find(contact => contact.name === newContact.name);
@@ -37,6 +45,11 @@ export function App() {
     contact.name.toLowerCase().includes(filter)
   );
 
+  const clearLocalStorage = () => {
+    localStorage.clear();
+    setContacts(initialContacts); // Очищаю локалсторидж, щоб на сторінці рендерилися контакти з initialContacts
+  };
+
   return (
     <div style={{ width: 500, marginLeft: 400, marginRight: 400 }}>
       <h2>Phonebook</h2>
@@ -47,6 +60,10 @@ export function App() {
         contacts={itemContacts}
         onDeleteContact={handleDeleteContact}
       />
+
+      <button type="button" onClick={clearLocalStorage}>
+        Скидання
+      </button>
     </div>
   );
 }
